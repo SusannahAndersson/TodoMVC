262ed3c468b444a9d56675e68ed3e0a50c88363c
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-02-20 13:11:23 +0100

    test 3 items mark one done

--- Test.GUI/Test.GUI/TodoItemTests.cs
+++ Test.GUI/Test.GUI/TodoItemTests.cs
@@ -73,7 +73,28 @@ namespace Test.GUI
             Assert.AreEqual(expected2, count2);
         }
 
-        
+        [Test]
+        public void Add3Items_MarkOneDone_Assert2Left()
+        {
+            int expected = 2;
+
+            _browser.Navigate().GoToUrl(_url);
+            var searchbox = _browser.FindElement(By.Id("input-bar"));
+            for (int itemIndex = 0; itemIndex < 3; itemIndex++)
+            {
+                searchbox.SendKeys("test " + itemIndex);
+                searchbox.SendKeys(Keys.Enter);
+            }
+
+            var list = _browser.FindElement(By.Id("todo-list"));
+            list.FindElement(By.CssSelector("li .toggle")).Click();
+            _browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+            var itemCount = _browser.FindElement(By.Id("unfinished-tasks"));
+            int count = int.Parse(itemCount.Text);
+
+            Assert.AreEqual(expected, count);
+        }
 
     }
 }
