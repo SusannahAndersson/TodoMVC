bd5b1da85a35e405abce24a0f67cb8d2d0e5aa53
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-02-22 06:20:16 +0100

    removed unneded comments from JS

--- TodoMVC/JavaScript.js
+++ TodoMVC/JavaScript.js
@@ -1,6 +1,4 @@
-﻿// toggle done marks last element always
-
-let template;
+﻿let template;
 let unfinishedTasks = 0;
 let finishedTasks = 0;
 let filterStatus = "all";
@@ -225,19 +223,3 @@ function updateTaskCount() {
         unfinishedTaskSpan.textContent = unfinishedTasks;
     }
 }
-
-/*
- Funktioner:
-    // *   Lägga till anteckningar.
-    // *   Ta bort anteckningar.
-    // *   Markera anteckningar som färdiga.
-    // *   Se hur många ofärdiga anteckningar som återstår ("X items left").
-    // *   Markera alla anteckningar som färdiga/ofärdiga (nedåtpilen till vänster om textfältet).
-    // *   Ta bort alla färdiga anteckningar ("Clear completed").
-    // *   Visa upp antingen alla anteckningar ("All"), alla ofärdiga anteckningar ("Active") eller alla färdiga anteckningar ("Completed").
-
-Ej:
-    *URL-hantering (att knapparna "All", "Active" och "Completed" ändrar på URL:en).
-    *Att anteckningar kan redigeras genom att dubbelklicka på dem.
-    *Automatiskt spara anteckningarna så att de finns kvar även när sidan laddas om.
- */
